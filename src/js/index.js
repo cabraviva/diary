221680ddb3472a9815772d5bdddc2c9f994df470
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2021-01-04 14:59:38 +0100

    Feelings

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,33 @@
 
 /* eslint no-unused-vars:1,one-var:0,space-before-function-paren:0,padded-blocks:0,no-trailing-spaces:0,no-multiple-empty-lines:0 */
 
+if (session.feelings === undefined) {
+  session.feelings = {}
+}
+
 const navbar = [
   {
     title: 'Home',
     name: 'Home',
     icon: 'home'
+  }
+]
+
+const feelings = [
+  {
+    icon: '<i class="fas fa-grin"></i>'
+  },
+  {
+    icon: '<i class="fas fa-smile"></i>'
+  },
+  {
+    icon: '<i class="fas fa-grimace"></i>'
+  },
+  {
+    icon: '<i class="fas fa-frown"></i>'
+  },
+  {
+    icon: '<i class="fas fa-angry"></i>'
   }
 ]
 
@@ -17,6 +39,21 @@ $$(document)(() => {
       <br /><br />
       <div class="feeling">
         <h3>Wie geht es dir gerade?</h3>
+        <div>
+          ${(() => {
+            let res = ''
+
+            for (const item of feelings) {
+              const choosed = session.feelings[new Date().toString().substr(0, 15)] || ''
+              // console.log(item, choosed, `<span onclick="$$('.feeling span i').removeClass('active');this.classList.add('active');session.feelings[new Date().toString().substr(0, 15)]='${item.icon.replace(/"/g, '\\"')}'" class="${choosed === item.icon ? 'active' : ''}">${item.icon}</span>`)
+              // res += `<span onclick="$$('.feeling span i').removeClass('active');this.classList.add('active');session.feelings[new Date().toString().substr(0, 15)]='${item.icon.replace(/"/g, '\\"')}'" class="${choosed === item.icon ? 'active' : ''}">${item.icon}</span>`
+
+              res += `<span onclick="for(const elem of $$('.feeling span')){elem.classList.remove('active')}this.classList.add('active');session.feelings[new Date().toString().substr(0, 15)]=this.innerHTML" class="${item.icon === choosed ? 'active' : ''}">${item.icon}</span>`
+            }
+
+            return res
+          })()}
+        </div>
       </div>
     </main>
     <nav>
