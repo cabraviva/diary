73d9eb40dd763c4c87e2e4b0ce48321cc7e8693a
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2021-01-04 10:19:34 +0100

    Greetings

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-/* global np, session, Blob */
+/* global $$, session */
 
 /* eslint no-unused-vars:1,one-var:0,space-before-function-paren:0,padded-blocks:0,no-trailing-spaces:0,no-multiple-empty-lines:0 */
+
+$$(document)(() => {
+  $$('body').innerHTML += `<h1>Hi <span><input type="text" oninput="session.name=this.value" value="${session.name || 'Wie heißt du?'}" /></span></h1>`
+})
